66f04160970aab5bdbb0e054f80d65d23e3202a4
TAkhamanov <takhamanov@beeline.kz>
2025-07-28 01:09:45 +0500

    add optional api key auth for server mode

--- models/mrrequest.go
+++ models/mrrequest.go
@@ -1,12 +1,13 @@
 package models
 
 type MRRequest struct {
+	DecoratorApiKey     string `json:"decorator_api_key"`
 	AuthToken           string `json:"auth_token" validate:"required"`
 	FilePath            string
-	ProjectId           int    `json:"project_id" validate:"required"`
-	JobId               int    `json:"job_id" validate:"required"`
+	ProjectId           int    `json:"project_id"`
+	JobId               int    `json:"job_id"`
 	ArtifactFormat      string `json:"format" validate:"required"`
-	ArtifactFileName    string `json:"artifact_file_name" validate:"required"`
+	ArtifactFileName    string `json:"artifact_file_name"`
 	MergeRequestIid     int    `json:"merge_request_iid" validate:"required"`
 	VulnerabilityMgmtId int    `json:"vulnerability_mgmt_id" validate:"required"`
 }
